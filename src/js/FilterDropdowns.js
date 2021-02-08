ba93f07f1898e0f1f7b772839589d6f4a56b346c
rishat <hrrkzn@yandex.ru>
2021-02-08 07:45:06 +0300

    08

--- src/js/FilterDropdowns.js
+++ src/js/FilterDropdowns.js
@@ -56,7 +56,20 @@ export default function FilterDropdowns() {
     //     });   
     // }
 
-    
+    const accordions = document.querySelectorAll('.accordion-item');
+
+    for(item of accordions) {
+        item.addEventListener('click', function() {
+            if(this.classList.contains('active')) {
+                this.classList.remove('active');
+            } else {
+                for(el of accordions) {
+                    el.classList.remove('active');
+                }
+                this.classList.add('active');
+            }
+        })
+    }
 
 
     // Клик в блоке Истории операций
