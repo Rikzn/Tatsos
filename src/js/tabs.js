1eff7ca2a3acdce15382f7963a17a528d4fe2cbd
Rikzn <41932726+Rikzn@users.noreply.github.com>
2021-03-01 18:10:14 +0300

    js-tabs

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,57 +1,80 @@
 // Таб на страницах продукции банка
+
 const tabsBtn = document.querySelectorAll(".js-tabs-link");
 const tabsItems = document.querySelectorAll(".js-tabs-item");
-
-tabsBtn.forEach(function(item) {
-    item.addEventListener("click", function() {
-        let currentBtn = item;
-        let tabId = currentBtn.getAttribute("data-tab");
-        let currentTab = document.querySelector(tabId);
-
-        if( ! currentBtn.classList.contains('sidebar-nav__link--active')) {
-            tabsBtn.forEach(function(item) {
-                item.classList.remove('sidebar-nav__link--active');
-            });
-            tabsItems.forEach(function(item) {
-                item.classList.remove('js-tabs-item--active');
-            });
-    
-            currentBtn.classList.add('sidebar-nav__link--active');
-            currentTab.classList.add('js-tabs-item--active');
-        }
-
-        
-    });
-});
-
-// Переключение между счетами
 const tabsBtnSection = document.querySelectorAll(".js-tabs-section-link");
 const tabsSection = document.querySelectorAll(".js-tabs-section");
 
-tabsBtnSection.forEach(function(section) {
-    section.addEventListener("click", function() {
-        let currentBtn = section;
+
+
+tabsBtnSection.forEach(function(BtnItem) {
+    BtnItem.addEventListener('click', function () {
+        let currentBtn = BtnItem;
         let tabId = currentBtn.getAttribute("data-tab");
         let currentTab = document.querySelector(tabId);
 
-        if( ! currentBtn.classList.contains('sidebar-nav__link--active')) {
-            tabsBtnSection.forEach(function(section) {
-                section.classList.remove('sidebar-nav__link--active');
-            });
-            tabsSection.forEach(function(section) {
-                section.classList.remove('js-tabs-section--active');
-            });
-            currentBtn.classList.add('sidebar-nav__link--active');
-            currentTab.classList.add('js-tabs-section--active');
+        if ( ! currentBtn.classList.contains('sidebar-nav__link--active')){
+            document.querySelector('.js-tabs-section--active').querySelector('.js-tabs-item:first-child').classList.add('js-tabs-item--active');
         }
-
-        
-    });
+    })
 });
 
 
 
-// Таб меняют картинку на странице "Настройка"
+
+// const tabsBtn = document.querySelectorAll(".js-tabs-link");
+// const tabsItems = document.querySelectorAll(".js-tabs-item");
+
+// tabsBtn.forEach(function(item) {
+//     item.addEventListener("click", function() {
+//         let currentBtn = item;
+//         let tabId = currentBtn.getAttribute("data-tab");
+//         let currentTab = document.querySelector(tabId);
+
+//         if( ! currentBtn.classList.contains('sidebar-nav__link--active')) {
+//             tabsBtn.forEach(function(item) {
+//                 item.classList.remove('sidebar-nav__link--active');
+//             });
+//             tabsItems.forEach(function(item) {
+//                 item.classList.remove('js-tabs-item--active');
+//             });
+//             currentBtn.classList.add('sidebar-nav__link--active');
+//             currentTab.classList.add('js-tabs-item--active');
+//         }
+
+        
+//     });
+// });
+
+// // Переключение между счетами
+// const tabsBtnSection = document.querySelectorAll(".js-tabs-section-link");
+// const tabsSection = document.querySelectorAll(".js-tabs-section");
+
+// tabsBtnSection.forEach(function(section) {
+//     section.addEventListener("click", function() {
+//         let tabsBtn = document.querySelector(".js-tabs-link");
+//         let currentBtn = section;
+//         let tabId = currentBtn.getAttribute("data-tab");
+//         let currentTab = document.querySelector(tabId);
+
+//         if( ! currentBtn.classList.contains('sidebar-nav__link--active')) {
+//             tabsBtnSection.forEach(function(section) {
+//                 section.classList.remove('sidebar-nav__link--active');
+//             });
+//             tabsSection.forEach(function(section) {
+//                 section.classList.remove('js-tabs-section--active');
+//             });
+//             currentBtn.classList.add('sidebar-nav__link--active');
+//             currentTab.classList.add('js-tabs-section--active');
+//         }
+
+        
+//     });
+// });
+
+
+
+// // Таб меняют картинку на странице "Настройка"
 const tabsLink = document.querySelectorAll(".sidebar-item");
 const tabsBg = document.querySelectorAll(".sidebar-img");
 
