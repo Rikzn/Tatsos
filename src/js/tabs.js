d05fd0c4a217ac1dde4dc5acfcb1cd06b62439a9
Rikzn <41932726+Rikzn@users.noreply.github.com>
2021-03-02 19:03:50 +0300

    еуые

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,63 +1,115 @@
-// Таб на страницах продукции банка
+var ratingsTabs = Array.prototype.slice.call(document.querySelectorAll('.js-ratings-tabs'));
 
-const tabsBtn = document.querySelectorAll(".js-tabs-link");
-const tabsItems = document.querySelectorAll(".js-tabs-item");
-const tabsBtnSection = document.querySelectorAll(".js-tabs-section-link");
-const tabsSection = document.querySelectorAll(".js-tabs-section");
+  ratingsTabs.forEach(function(element) {
+    var links = Array.prototype.slice.call(element.querySelectorAll('.company-ratings__tabs-nav-link'));
+    var items = Array.prototype.slice.call(element.querySelectorAll('.company-ratings__tabs-item'));
 
+    if (links.length !== items.length) {
+      console.error('Not equal amount of elements');
+      return;
+    }
 
+    function setActiveTab(index) {
+      links.forEach(function(link) {
+        link.classList.remove('active');
+      })
+      items.forEach(function(item) {
+        item.classList.remove('active');
+      })
 
-tabsBtnSection.forEach(function(BtnItem) {
-    BtnItem.addEventListener('click', function () {
-        let currentBtn = BtnItem;
-        let tabId = currentBtn.getAttribute("data-tab");
-        let currentTab = document.querySelector(tabId);
+      links[index].classList.add('active');
+      items[index].classList.add('active');
+    }
 
-        if ( ! currentBtn.classList.contains('sidebar-nav__link--active')){
-            document.querySelector('.js-tabs-section--active').querySelector('.js-tabs-item:first-child').classList.add('js-tabs-item--active');
-        }
+    links.forEach(function(link, linkIndex) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        setActiveTab(linkIndex)
+      })
     })
-});
+
+    setActiveTab(0);
+  })
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Таб на страницах продукции банка
 
 // const tabsBtn = document.querySelectorAll(".js-tabs-link");
 // const tabsItems = document.querySelectorAll(".js-tabs-item");
+// const tabsBtnSection = document.querySelectorAll(".js-tabs-section-link");
+// const tabsSection = document.querySelectorAll(".js-tabs-section");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // tabsBtn.forEach(function(item) {
 //     item.addEventListener("click", function() {
 //         let currentBtn = item;
 //         let tabId = currentBtn.getAttribute("data-tab");
 //         let currentTab = document.querySelector(tabId);
-
-//         if( ! currentBtn.classList.contains('sidebar-nav__link--active')) {
-//             tabsBtn.forEach(function(item) {
-//                 item.classList.remove('sidebar-nav__link--active');
-//             });
-//             tabsItems.forEach(function(item) {
-//                 item.classList.remove('js-tabs-item--active');
-//             });
-//             currentBtn.classList.add('sidebar-nav__link--active');
-//             currentTab.classList.add('js-tabs-item--active');
-//         }
-
-        
+//             if( ! currentBtn.classList.contains('sidebar-nav__link--active')) {
+//                 tabsBtn.forEach(function(item) {
+//                     item.classList.remove('sidebar-nav__link--active');
+//                     });
+//                 tabsItems.forEach(function(item) {
+//                     item.classList.remove('js-tabs-item--active');
+//                     });
+//                 currentBtn.classList.add('sidebar-nav__link--active');
+//                 currentTab.classList.add('js-tabs-item--active');
+//                 }
 //     });
 // });
 
-// // Переключение между счетами
-// const tabsBtnSection = document.querySelectorAll(".js-tabs-section-link");
-// const tabsSection = document.querySelectorAll(".js-tabs-section");
 
-// tabsBtnSection.forEach(function(section) {
-//     section.addEventListener("click", function() {
-//         let tabsBtn = document.querySelector(".js-tabs-link");
-//         let currentBtn = section;
-//         let tabId = currentBtn.getAttribute("data-tab");
+
+// tabsBtnSection.forEach(function(BtnItem) {
+//     BtnItem.addEventListener('click', function() {
+//         let currentBtn = BtnItem;
+//         let tabId = currentBtn.getAttribute('data-tab');
 //         let currentTab = document.querySelector(tabId);
-
-//         if( ! currentBtn.classList.contains('sidebar-nav__link--active')) {
+//         let tabsItem = document.querySelector('.js-tabs-item');
+//         if (!currentBtn.classList.contains('sidebar-nav__link--active')) {
+//             document.querySelector('.js-tabs-section--active').querySelector('.js-tabs-item:first-child').classList.add('js-tabs-item--active');
 //             tabsBtnSection.forEach(function(section) {
 //                 section.classList.remove('sidebar-nav__link--active');
 //             });
@@ -66,11 +118,13 @@ tabsBtnSection.forEach(function(BtnItem) {
 //             });
 //             currentBtn.classList.add('sidebar-nav__link--active');
 //             currentTab.classList.add('js-tabs-section--active');
+//             // tabsItem.classList.add('js-tabs-item--active');
 //         }
-
-        
 //     });
 // });
+
+
+
 
 
 
